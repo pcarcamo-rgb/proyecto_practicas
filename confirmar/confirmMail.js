f5580e4d9844087304e2b40d61892a2bdf795c2f
pcarcamo-rgb <pablittocarcamo@gmail.com>
2023-11-03 19:23:40 -0300

    peticion api  para confirmar Email

--- confirmar/confirmMail.js
+++ confirmar/confirmMail.js
@@ -1,9 +1,23 @@
-const urlParams = new URLSearchParams(window.location.search);
-        
-const nombre = urlParams.get('idUser');
-
-function confirmMail (idUser){
-    fetch('https://gregarious-raindrop-0fb73e.netlify.app/verifyEmail?userId='+idUser,{
-        method:"PUT",
-    })
-}
+function confirmMail (){
+    const urlParams = new URLSearchParams(window.location.search);   
+    const userId = urlParams.get('userId')
+        console.log(userId)
+    console.table(urlParams)
+    fetch(`https://gregarious-raindrop-0fb73e.netlify.app/verifyEmail?userId=${userId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Respuesta:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
